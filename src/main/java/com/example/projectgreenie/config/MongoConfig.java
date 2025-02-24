927d49982bbd8d6cc138e2aa2193f849afa83624
Akila Lakshitha <136437833+mrakiyaaa@users.noreply.github.com>
2025-02-25 01:15:49 +0530

    chore(application.yml): Create Yml file

--- src/main/java/com/example/projectgreenie/config/MongoConfig.java
+++ src/main/java/com/example/projectgreenie/config/MongoConfig.java
@@ -1,5 +1,7 @@
 package com.example.projectgreenie.config;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
