57063f050768d75106c0c09e9f689835fdf2ffb7
Janudi Meegoda <137209357+djdJANU@users.noreply.github.com>
2025-04-18 03:32:30 +0530

    feat(model): add ReportedPost model for managing reported posts

--- src/main/java/com/example/projectgreenie/model/ReportedPost.java
+++ src/main/java/com/example/projectgreenie/model/ReportedPost.java
@@ -1,5 +1,24 @@
 package com.example.projectgreenie.model;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(collection = "reported_posts")
 public class ReportedPost {
+
+    @Id
+    private String reportId;
+    private String postId;
+    private String reportedBy;
+    private String reason;
+    private LocalDateTime timestamp;
 
 }
